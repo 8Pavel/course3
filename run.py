f87a6bb7289b64a7eb662f7f98b9bc41740ce970
8Pavel <seliv.pavel8@gmail.com>
2022-06-01 15:04:23 +0500

    Версия проекта "курсовая3"

---
+++ run.py
@@ -0,0 +1,29 @@
+import logging
+from json import JSONDecodeError
+
+from flask import Flask, abort
+
+from app.html.views import html_bp
+from app.api.views import api_bp
+from app.logger import create_logger
+
+create_logger()
+logger = logging.getLogger('basic')
+
+app = Flask(__name__)
+
+
+app.register_blueprint(html_bp)
+app.register_blueprint(api_bp)
+
+
+@app.errorhandler(FileNotFoundError)
+@app.errorhandler(JSONDecodeError)
+def bad_file(error):
+    logger.error('Получена ошибка {str(error)}')
+    abort(500)
+
+
+if __name__ == "__main__":
+    logger.info('Приложение стартует')
+    app.run()
